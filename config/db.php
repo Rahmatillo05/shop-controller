5cefbdd836da00c427d89fedaf034954a7356de3
Rahmatillo05 <hrrahmatillo2oo5@gmail.com>
2025-01-17 09:00:50 +0500

    console prod

--- config/db.php
+++ config/db.php
@@ -1,12 +1,13 @@
 <?php
 $connection = env('DB_CONNECTION', 'mysql');
 $host = env('DB_HOST', 'localhost');
+$port = env('DB_PORT', '3306');
 $username = env('DB_USERNAME', 'root');
 $password = env('DB_PASSWORD', '');
 $dbname = env('DB_DATABASE', 'shop');
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => "$connection:host=$host;dbname=$dbname",
+    'dsn' => "$connection:host=$host;port=$port;dbname=$dbname",
     'username' => "$username",
     'password' => "$password",
     'charset' => 'utf8',
